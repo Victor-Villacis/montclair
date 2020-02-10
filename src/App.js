2a8a2338c6c243e134c8d7ee9d30a6a5fe47a627
Cibelle Montor <you@example.com>
2020-02-09 21:46:31 -0700

    added dropdown to nav link

--- src/App.js
+++ src/App.js
@@ -1,9 +1,5 @@
 import React from "react";
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-} from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Nav from "./components/Nav";
 import Team from "./pages/Team/Team";
 import News from "./pages/News/News";
@@ -11,25 +7,21 @@ import Contact from "./pages/Contact/Contact";
 import Login from "./pages/Login/Login";
 import About from "./pages/About/About";
 
-
-
 // import "./App.css";
-
 
 export default function App() {
   return (
-<Router>
-  <div>
-    <Nav />
-    <Switch>
-      <Route exact path="/" component={Team} />
-      <Route exact path="/news" component={News} />
-      <Route exact path="/contact" component={Contact} />
-      <Route exact path="/login" component={Login} />
-      <Route exact path="/about" component={About} />
-    </Switch>
-  </div>
-
-</Router>
+    <Router>
+      <div>
+        <Nav />
+        <Switch>
+          <Route exact path="/about" component={About} />
+          <Route exact path="/" component={Team} />
+          <Route exact path="/news" component={News} />
+          <Route exact path="/contact" component={Contact} />
+          <Route exact path="/login" component={Login} />
+        </Switch>
+      </div>
+    </Router>
   );
 }
--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -1,41 +1,74 @@
-import React from "react";
+import React, { Component } from "react";
 import "./style.css";
-function Nav() {
-  return (
-    <nav className="nav-logout-view">
-      <div class="nav-wrapper">
-        <a href="https://montclaircareshare.com/">
-          <img
-            id="logo"
-            src={require("./images/mcslogo.jpg")}
-            alt="MontClair Care Share"
-          />
-        </a>
-        <ul id="nav-mobile" className="right hide-on-med-and-down nav-links">
-          <li>
-            <a href="/">Team</a>
-            {/* <Link to="/">Team</Link> */}
-          </li>
-          <li>
-            <a href="/news">News</a>
-           {/* <Link to="/news">News</Link> */}
-          </li>
-          <li>
-            <a href="/contact">Contact Us</a>
-            {/* <Link to="/contact">Contact Us</Link> */}
-          </li>
-          <li>
-            < a href = "/login" >Login</a>
-            {/* <Link to="/login">Login</Link> */}
-          </li>
-          <li>
-            < a href = "/about" >About</a>
-            {/* <Link to="/about">About</Link> */}
-          </li>
-        </ul>
+import M from "materialize-css";
+
+//set up the nav bar with a dropdown
+
+class Nav extends Component {
+  componentDidMount() {
+    document.addEventListener("DOMContentLoaded", function() {
+      var elems = document.querySelectorAll(".dropdown-trigger");
+
+      M.Dropdown.init(elems, {
+        inDuration: 500,
+        outDuration: 505,
+        gutter: 100,
+        hover: true,
+        coverTrigger: false
+      });
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <nav className="nav-logout-view">
+          <a href="https://montclaircareshare.com/">
+            <img
+              id="logo"
+              src={require("./images/mcslogo.jpg")}
+              alt="MontClair Care Share"
+            />
+          </a>
+
+          <ul id="nav-mobile" className="right hide-on-med-and-down nav-links">
+            <li>
+              <a className="dropdown-trigger  " data-target="dropdown1">
+                About <i class="fas fa-caret-down "></i>
+              </a>
+            </li>
+            <li>
+              <a href="/news">News</a>
+              {/* <Link to="/news">News</Link> */}
+            </li>
+            <li>
+              <a href="/contact">Contact Us</a>
+              {/* <Link to="/contact">Contact Us</Link> */}
+            </li>
+            <li>
+              <a href="/login">Login</a>
+              {/* <Link to="/login">Login</Link> */}
+            </li>
+          </ul>
+
+          {/* Dropdown content  */}
+          <ul
+            id="dropdown1"
+            className="dropdown-content nav-links right hide-on-med-and-down  "
+          >
+            <li className="sub-link">
+              <a href="/">Team</a>
+              {/* <Link to="/">Team</Link> */}
+            </li>
+            <li className="sub-link">
+              <a href="/">Sponsors</a>
+              {/* <Link to="/">Team</Link> */}
+            </li>
+          </ul>
+        </nav>
       </div>
-    </nav>
-  );
+    );
+  }
 }
 
 export default Nav;
